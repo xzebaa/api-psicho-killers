ad99d68a7048c3a146a998f94b4305eb853a0467
israelpeanut <israel.lazo@externos-cl.cencosud.com>
2021-08-25 08:33:34 -0400

    changed config to take mongoDB url from env

--- server/config/config.js
+++ server/config/config.js
@@ -5,11 +5,7 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 // mongodb url
-let urlDB = "lero lero";
-
-if (process.env.NODE_ENV === "production") {
-  urlDB = process.env.MONGO_URI;
-}
+let urlDB = process.env.MONGO_URI;
 process.env.URLDB = urlDB;
 
 // venciomiento del token: 60 segun * 60 minutos * 24 horas * 30 dias
