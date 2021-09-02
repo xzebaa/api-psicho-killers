a393595ed2e77d2a95e91a4402d9c32c0ca5d209
Sebastian Alvarez <ext_salvarezl@falabella.cl>
2021-09-02 02:12:49 -0400

    --no-edit

--- server/config/config.js
+++ server/config/config.js
@@ -6,13 +6,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 // mongodb url
 
-urlDB =
-  "mongodb+srv://killer-user:089as7df983234@cluster0.korju.mongodb.net/psychoKiller";
+urlDB = "";
 
 process.env.URLDB = urlDB;
 
 // venciomiento del token: 60 segun * 60 minutos * 24 horas * 30 dias
 process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 
-process.env.SENDGRID_API_KEY =
-  "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";
+process.env.SENDGRID_API_KEY = "";
