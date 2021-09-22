c55481d7374c8031f9ef986d50e2ccfc53cf7e89
Sebastian Alvarez <sebastian.alvarez@peanuthub.cl>
2021-09-22 18:16:32 -0300

    fix db

--- server/config/config.js
+++ server/config/config.js
@@ -4,12 +4,6 @@ process.env.PORT = process.env.PORT || 3001;
 // entorno
 process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
-// mongodb url
-
-urlDB = "";
-
-process.env.URLDB = urlDB;
-
 // venciomiento del token: 60 segun * 60 minutos * 24 horas * 30 dias
 process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 
